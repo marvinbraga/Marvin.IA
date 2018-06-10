4c5c2ead96d3308606cd9b3ff6fbc2b7e1a76f37
marvinbraga <mvbraga@gmail.com>
2018-06-09 22:05:27 -0300

    Poc #14

--- PoC/Marvin.PoC.IA.StartUI.pas
+++ PoC/Marvin.PoC.IA.StartUI.pas
@@ -1,5 +1,29 @@
 unit Marvin.PoC.IA.StartUI;
 
+{
+  MIT License
+
+  Copyright (c) 2018 Marcus Vinicius D. B. Braga
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+}
+
 interface
 
 uses
@@ -69,7 +93,7 @@ begin
       { load pure data file }
       LStream.LoadFromFile(DlgData.FileName);
 
-      { recupera os dados convertidos }
+      { transforma os dados originais para o formato compatível e ajustado }
       TIrisDataConverter.New(LStream.DataString).Execute(LIrisInputData, LIrisOutputData);
       { faz o split dos dados para treino e teste }
 
